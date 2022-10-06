997f9ff5a8e0766f16a783a99707fbd2de60edff
“Marcela <mpmontiela@gmail.com>
2022-10-06 11:18:52 +0200

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+  if(number1 > number2) {
+    console.log("number1", number1);
+    return number1;
+  } else {
+    return number2;
+  }
+  }
+
 
 
 
